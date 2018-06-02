914aa11a197a9d4c112469b807ebe319334d3836
Yuexiang Gao <yxgao@thoughtworks.com>
2018-06-02 20:39:09 +0800

    make point stay 0 when reduce it and it is 0

--- src/main/kotlin/com/thoughtworks/rldd/service/member/model/User.kt
+++ src/main/kotlin/com/thoughtworks/rldd/service/member/model/User.kt
@@ -2,9 +2,8 @@ package com.thoughtworks.rldd.service.member.model
 
 class User(var id: String, var name: String, var point: Int) {
   fun reducePoint() {
-    point -= 1
-    if (point == 0) {
-      restorePoint()
+    if (point > 0) {
+      --point
     }
   }
 
